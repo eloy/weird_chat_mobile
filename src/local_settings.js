656b2910f8da72cbcfaa413d6543a791eaa8434c
Eloy Gomez <eloy@indeos.es>
2026-02-20 16:58:10 +0100

    Fix issues with getAll

--- src/local_settings.js
+++ src/local_settings.js
@@ -1,4 +1,3 @@
-
 import AsyncStorage from '@react-native-async-storage/async-storage';
 export default class LocalSetting  {
   static set(key, value) {
@@ -10,6 +9,12 @@ export default class LocalSetting  {
   }
 
   static getAll(keys) {
-    return AsyncStorage.multiGet(keys);
+    return AsyncStorage.multiGet(keys).then(values => {
+      let data = {}
+      for (let value of values) {
+        data[value[0]] = value[1];
+      }
+      return data;
+    });
   }
 }
